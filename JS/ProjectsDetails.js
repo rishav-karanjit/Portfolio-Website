4cc6bc93912dffbbffad2c1cf5d266344b86a655
Rishav Karanjit <karanjitrishav4@gmail.com>
2022-09-25 17:45:49 -0400

    Added details to project and past experiences

---
+++ JS/ProjectsDetails.js
@@ -0,0 +1,47 @@
+function onGoogle() {
+    document.getElementById("overlayGoogle").style.display = "block";
+}
+
+function offGoogle() {
+    document.getElementById("overlayGoogle").style.display = "none";
+}
+
+function onPokemon() {
+    document.getElementById("overlayPokemon").style.display = "block";
+}
+
+function offPokemon() {
+    document.getElementById("overlayPokemon").style.display = "none";
+}
+
+function onAES() {
+    document.getElementById("overlayAES").style.display = "block";
+}
+
+function offAES() {
+    document.getElementById("overlayAES").style.display = "none";
+}
+
+function onJobify() {
+    document.getElementById("overlayJobify").style.display = "block";
+}
+
+function offJobify() {
+    document.getElementById("overlayJobify").style.display = "none";
+}
+
+function onGRA() {
+    document.getElementById("overlayGRA").style.display = "block";
+}
+
+function offGRA() {
+    document.getElementById("overlayGRA").style.display = "none";
+}
+
+function onIntern() {
+    document.getElementById("overlayIntern").style.display = "block";
+}
+
+function offIntern() {
+    document.getElementById("overlayIntern").style.display = "none";
+}
