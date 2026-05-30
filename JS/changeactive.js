2cbd0f7705699c3ebb6f7dd2d30de64cd7c9ea68
Rishav Karanjit <karanjitrishav4@gmail.com>
2026-05-29 20:41:41 -0700

    rm all

--- JS/changeactive.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $('.nav li a').click(function(e) {
-        $('.nav li a.active').removeClass('active');
-
-        var $parent = $(this);
-        $parent.addClass('active');
-    });
-});
